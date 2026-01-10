c6f337e972266e6d8519e7a35c2290dc7b4c5f66
joshphillis <joshphillis@hotmail.com>
2026-01-10 16:31:38 -0600

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 
@@ -12,7 +12,7 @@ terraform {
     resource_group_name  = var.workload_state_rg
     storage_account_name = var.workload_state_sa
     container_name       = var.workload_state_container
-    key                  = "workload-vm-stack.tfstate"
+    key                  = var.workload_state_key
   }
 }
 
